6c1175084818b61d76c133ab4e1632ec8e52c9c4
Volchonok82 <volchonok82@users.noreply.github.com>
2020-04-19 13:08:52 +0300

    metrik

--- template/footer.php
+++ template/footer.php
@@ -7,12 +7,15 @@ $year = strftime('%Y');
     <div class="container text-center">
         <div class="row align-items-center">
             <div class="meleo col-sm-2 text-left pt-2">
-                <p>© MELEO Production 2007-<?= $year?></p>
+                <p>© MELEO Production 2007-<?= $year ?></p>
             </div>
-            <div class="mailmetrik col">
+            <div class="metrik col">
                 <!-- <a href="http://top.mail.ru/jump?from=1702789" target="_blank"> 
                     <img src="//top-fwz1.mail.ru/counter?id=1702789;t=131" alt="Рейтинг@Mail.ru">
                 </a> -->
+                <!-- Yandex.Metrika informer -->
+                <a href="https://metrika.yandex.ru/stat/?id=62073865&amp;from=informer" target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/62073865/3_0_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+                <!-- /Yandex.Metrika informer -->
             </div>
             <div class="host col">
                 <a href="http://www.jino.ru" target="_blank">
@@ -46,14 +49,9 @@ $year = strftime('%Y');
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- template/header.php
+++ template/header.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="ru">
+
 <head>
     <title>Паралет Paraplana.net</title>
     <!-- Required meta tags -->
@@ -7,16 +8,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="images/propeller.png">
     <script src="https://kit.fontawesome.com/32bd5186c0.js" crossorigin="anonymous"></script>
-</head>
-<body>
+    <!-- Yandex.Metrika counter -->
+    <script>
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-<!-- Rating Mail.ru counter -->
-<!-- <script>
+        ym(62073865, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/62073865" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+</head>
+
+<body>
+    <!-- Rating Mail.ru counter -->
+    <script>
     var _tmr = window._tmr || (window._tmr = []);
     _tmr.push({id: "1702789", type: "pageView", start: (new Date()).getTime()});
     (function (d, w, id) {
@@ -26,64 +47,63 @@
         var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
         if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
     })(document, window, "topmailru-code");
-</script><noscript><div>
+    </script><noscript><div>
         <img src="https://top-fwz1.mail.ru/counter?id=1702789;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
-    </div></noscript> -->
- <!-- //Rating Mail.ru counter -->
+    </div></noscript>
+    <!-- //Rating Mail.ru counter -->
+  
+    <header class="site-header" id="site-header">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-sm-2">
+                    <img class="mini-paralet" src="images/paralet_mini.gif" alt="иконка паралета">
+                </div>
+                <div class="col-sm-10">
+                    <h1 class="text-center">Паралеты и парапланы на Paraplana.net</h1>
+                </div>
 
+                <div class="col-sm-12">
+                    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(170,220,255,0.2)">
+                        <a class="navbar-brand" href="index.php" title="На главную">Paraplana.net</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-<header class="site-header" id="site-header">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-sm-2">
-                <img class="mini-paralet" src="images/paralet_mini.gif" alt="иконка паралета">
-            </div>
-            <div class="col-sm-10">
-                <h1 class="text-center">Паралеты и парапланы на Paraplana.net</h1>
-            </div>
-
-            <div class="col-sm-12">
-                <nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgba(170,220,255,0.2)">
-                    <a class="navbar-brand" href="index.php" title="На главную">Paraplana.net</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="paralet.php">Паралеты</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="paraplan.php">Парапланы</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="spare-parts.php">Запчасти</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="training.php">Обучение</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="photo.php">Фото/Видео</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Контакты</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Интересное
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: rgba(170,220,255,0.8)" >
-                                    <a class="dropdown-item" href="http://paraplan.ru" target="_blank">Форумы Paraplan.ru</a>
-                                    <a class="dropdown-item" href="http://airsport.ru" target="_blank">Air Sport Russia</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="articles.php">Полезные статьи</a>
-                                </div>
-                        </ul>
-                    </div>
-                </nav>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="paralet.php">Паралеты</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="paraplan.php">Парапланы</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="spare-parts.php">Запчасти</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="training.php">Обучение</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="photo.php">Фото/Видео</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.php">Контакты</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Интересное
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: rgba(170,220,255,0.8)">
+                                        <a class="dropdown-item" href="http://paraplan.ru" target="_blank">Форумы Paraplan.ru</a>
+                                        <a class="dropdown-item" href="http://airsport.ru" target="_blank">Air Sport Russia</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="articles.php">Полезные статьи</a>
+                                    </div>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </div>
-    </div>
 
-</header>
+    </header>
